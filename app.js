14932b13bdf74e1cf7f3eca99de6dfbe00202ad3
Saeed <sa.ghofraniivari@gmail.com>
2022-04-17 12:08:21 +0430

    database

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 var express = require('express');
 var logger = require('morgan');
+const createError = require('http-errors');
 
 var appRouter = require('./routes/app.route');
+/**
+ * database connection
+ */
+require("./db/connection.db");
 
 var app = express();
 
@@ -10,5 +15,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', appRouter);
+
+/**
+ * error handler
+ */
+app.use((_req, _res, next) => { next(createError(404)); });
+app.use((err, req, res, _next) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 404);
+    res.status(404).json({ path: req.originalUrl, success: false, result: { error: "Path does`nt exist" } });
+
+    // uncaught Exception handler
+    process.on("uncaughtException", (err) => {
+        console.log("UNCAUGHT EXCEPTION, APP SHUTTING DOWN NOW!!");
+        console.log(err.message, err.name);
+        res.status(500).json({ path: req.originalUrl, success: false, result: { error: "Internal server error" } });
+        process.exit(1);
+    });
+});
 
 module.exports = app;
